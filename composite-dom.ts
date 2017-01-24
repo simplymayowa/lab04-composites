e0d9c689f334142ec02bee165a1a005123deccea
Mayowa Aina <maina12@uw.edu>
2017-01-24 11:20:54 -0800

    completed composite lab

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,55 @@ interface IDomElement {
   print();
 }
 
+class DomElement implements IDomElement {
+  elementName: string;
+  elements: IDomElement[];
+  text: string;  
+
+  constructor(elementName: string, text?: string) {
+    this.elementName = elementName; 
+    this.elements = []; 
+    this.text = text;  
+  }
+
+  add(element: IDomElement) {
+    this.elements.push(element); 
+  }
+
+  print() {
+    console.log(`<${this.elementName}>`); 
+    if(this.text) {
+      console.log(this.text)
+    }
+    for (let element of this.elements) {
+      element.print();
+    }
+    console.log(`</${this.elementName}>`)
+  }
+}
+
+class TextNode implements IDomElement {
+  text: string; 
+  
+  constructor(text: string) {
+    this.text = text;
+  }  
+  
+  print () {
+    console.log(this.text); 
+  }
+}
+
+let p1 = new DomElement("p"); 
+let p2 = new DomElement("p"); 
+let div = new DomElement("div"); 
+let html = new DomElement("html"); 
+
+p1.add(new TextNode("Hi, I am a TextNode being printed")); 
+p2.add(new TextNode("TextNode == leaf")); 
+
+div.add(p1); 
+div.add(p2); 
+html.add(div);
+
+html.print(); 
